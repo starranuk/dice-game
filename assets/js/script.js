2ad7ec9368cdfa9ac777ecc8061f5f3e18a32c20
starranuk <starran@live.co.uk>
2021-12-15 12:18:55 +0000

    popup added, needs refining

--- assets/js/script.js
+++ assets/js/script.js
@@ -291,3 +291,9 @@ function incrementWrongAnswer() {
     document.getElementById("incorrect").innerText = rollingScore;
     
 }
+
+// When the user clicks on div, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
